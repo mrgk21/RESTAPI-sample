1de3f17f79647cb851aa59980a38e94cbfc5034b
Gaurav Kelkar <gauravkelkar35@gmail.com>
2022-12-04 10:35:18 +0530

    Added jwt access and refresh tokens for login(WIP)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,14 +1,10 @@
 import express, { Request } from "express";
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import path from "path";
+import jwt from "jsonwebtoken";
+import _ from "lodash";
 
 const authRouter = express.Router();
-
-type AccessToken = {
-	access_token: string;
-	scope: string;
-	token_type: string;
-};
 
 const utils = {
 	buildPath: (location: string): string => {
@@ -20,17 +16,50 @@ const utils = {
 	},
 };
 
+let refreshTokens: string[] = [];
+const testAcc: object = {
+	user: "test",
+	pass: "123",
+};
+
 // future updates
 type OAuthLoginSite = "github" | "google";
+
+authRouter.get("/info", (req: Request<{}, {}, {}, { token: string }>, res) => {
+	const { token } = req.query;
+	jwt.verify(token, String(process.env.JWT_ACCESS_TOKEN_SECRET), (err) => {
+		if (err) return res.status(403).send(err.message);
+		return res.status(200).send("You got in!");
+	});
+});
+
+// @ts-ignore
+authRouter.get("/refresh", (req: Request<{}, {}, {}, { token: string }>, res) => {
+	const { token } = req.query;
+	if (!refreshTokens.includes(token)) return res.sendStatus(403);
+	jwt.verify(token, String(process.env.JWT_REFRESH_TOKEN_SECRET), (err, user) => {
+		if (err) return res.status(403).send(err.message);
+		const accessToken = jwt.sign({ user }, String(process.env.JWT_ACCESS_TOKEN_SECRET));
+		return res.status(200).send({ message: "you got in!", token: accessToken });
+	});
+});
 
 authRouter.get("/login", (req: Request<{}, {}, {}, { entryPage: string }>, res) => {
 	const { entryPage } = req.query;
 
-	if (typeof entryPage == "string") if (entryPage == "true") return res.sendFile(utils.buildPath("pages/login.html"));
+	if (typeof entryPage == "string")
+		if (entryPage == "true") return res.sendFile(utils.buildPath("pages/login.html"));
 });
 
 authRouter.post("/login", (req: Request<{}, {}, { user: string; pass: string }, {}>, res) => {
 	const { user, pass } = req.body;
+	if (!_.isEqual({ user, pass }, testAcc)) return res.status(403).send("wrong username/password");
+	const accessToken = jwt.sign({ user }, String(process.env.JWT_ACCESS_TOKEN_SECRET), {
+		expiresIn: "15s",
+	});
+	const refreshToken = jwt.sign({ user }, String(process.env.JWT_REFRESH_TOKEN_SECRET));
+	refreshTokens.push(refreshToken);
+	return res.send({ accessToken, refreshToken });
 });
 
 authRouter.get("/github", (req, res) => {
@@ -38,37 +67,39 @@ authRouter.get("/github", (req, res) => {
 	res.redirect(url);
 });
 
-const getUser = async (tokenType: string, token: string): Promise<object> => {
+const getUser = async (token: string): Promise<object> => {
 	const { data } = await axios.get("https://api.github.com/user", {
 		headers: {
-			Authorization: `${tokenType} ${token}`,
+			Authorization: `bearer ${token}`,
 			"Accept-encoding": "application/json",
 		},
 	});
 	return data;
 };
 
-const getAccessToken = async (code: string): Promise<AccessToken> => {
+const getAccessToken = async (code: string): Promise<string | any> => {
 	const getTokenObj = {
 		client_id: process.env.GITHUB_CLIENT_ID,
 		client_secret: process.env.GITHUB_CLIENT_SECRET,
 		code,
 	};
-	const { data } = await axios.post(`https://github.com/login/oauth/access_token`, getTokenObj, {
+	const {
+		data: { access_token },
+	} = await axios.post(`https://github.com/login/oauth/access_token`, getTokenObj, {
 		headers: {
 			Accept: "application/json",
 			"Accept-encoding": "application/json",
 		},
 	});
-	return data;
+	return access_token;
 };
 
 authRouter.get("/github/callback", async (req: Request<{}, {}, {}, { code: string }, {}>, res) => {
 	const { code } = req.query;
-	const accessTokenObj: AccessToken = await getAccessToken(code);
+	const accessToken = await getAccessToken(code);
 
-	const githubData = await getUser(accessTokenObj.token_type, accessTokenObj.access_token);
-	res.send(githubData);
+	const githubData = await getUser(accessToken);
+	return res.send(githubData);
 });
 
 export default authRouter;
