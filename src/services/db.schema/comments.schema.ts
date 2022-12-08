30e095977d578c830b8fb6a3fdb31d9ce9524ebe
Gaurav Kelkar <gauravkelkar35@gmail.com>
2022-12-08 21:03:57 +0530

    Changed schema names. Corrected comment schema typo

--- src/services/db.schema/comments.schema.ts
+++ src/services/db.schema/comments.schema.ts
@@ -3,16 +3,16 @@ import { Schema } from "mongoose";
 interface commentInterface {
 	postId: number;
 	id: number;
+	name: string;
 	email: string;
-	title: string;
 	body: string;
 }
 
 const commentSchema = new Schema<commentInterface>({
 	postId: { type: Number, required: true },
 	id: { type: Number, required: true, unique: true },
+	name: { type: String, required: true },
 	email: { type: String, required: true },
-	title: { type: String, required: true },
 	body: { type: String, default: "" },
 });
 
