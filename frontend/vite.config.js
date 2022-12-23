89ac397a20c4c830f930c4087420803f2cf09708
Gaurav Kelkar <gauravkelkar35@gmail.com>
2022-12-23 19:41:15 +0530

    Added nginx as reverse-proxy between backend and frontend. fixes #2.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command, mode }) => {
 	const env = loadEnv(mode, process.cwd(), "");
 	return {
+		server: {
+			host: "0.0.0.0",
+			port: 5000,
+		},
 		preview: {
 			host: "0.0.0.0",
 			port: env.PORT,
